1978e9478a8c0d58ec3b70d502dd8d13fe710244
2594523286@qq.com <2594523286@qq.com>
2020-02-29 00:35:56 +0800

    添加来源：JWT 生成Token及验证PDF

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # springboot-jwt
 SpringBoot集成JWT实现token验证
 
-### 博客地址：https://www.jianshu.com/p/e88d3f8151db
+### 参考代码：在resources目录下：JWT 生成Token及验证
+### 参考简书：https://www.jianshu.com/p/e88d3f8151db
--- src/main/java/com/example/demo/Common/JwtTokenUtil.java
+++ src/main/java/com/example/demo/Common/JwtTokenUtil.java
@@ -63,4 +63,67 @@ public class JwtTokenUtil {
         }
     }
 
-    }
+}
+
+/*//resources--JWT生成TOKEN及验证.pdf
+//公用密钥-保存在服务端,客户端是不会知道密钥的,以防被攻击
+public static String SECRET = "ThisIsASecret";
+//生成Troke
+public static String createToken(String id,String password) {
+        //签发时间
+        Date iatDate = new Date();
+        //过地时间  1分钟后过期
+        Calendar nowTime = Calendar.getInstance();
+        nowTime.add(Calendar.MINUTE, 1);
+        Date expiresDate = nowTime.getTime();
+        Map<String, Object> map = new HashMap();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+        String token = JWT.create()
+                    .withHeader(map)
+                    .withClaim( "name","Free码生") //设置 载荷 Payload
+                    .withClaim("age","12")
+                    .withClaim( "org","测试")
+                    .withExpiresAt(expiresDate)//设置过期时间,过期时间要大于签发时间
+                    .withIssuedAt(iatDate)//设置签发时间
+                    .sign(Algorithm.HMAC256(SECRET));//加密
+        return token;
+}
+//校验TOKEN
+public static boolean verifyToken(String token,String password) throws UnsupportedEncodingException{
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
+        try {
+            verifier.verify(token);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+}
+//获取Token信息
+public static DecodedJWT getTokenInfo(String token) throws UnsupportedEncodingException{
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
+        try{
+            return verifier.verify(token);
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
+}
+//测试
+public static void main(String[] args) throws Exception {
+        //生成token
+        String token = JwtTokenUtil.createToken();
+        System.out.println("token:" + token);
+        //校验token
+        Boolean verify = JwtTokenUtil.verifyToken(token);
+        System.out.println("token 校验结果:" + verify);
+        //获取Token信息
+        DecodedJWT tokenInfo = JwtTokenUtil.getTokenInfo(token);
+        String alg = tokenInfo.getHeaderClaim("alg").asString();
+        System.out.println("alg:"+alg);
+        Map<String, Claim> claims = tokenInfo.getClaims();
+        System.out.println("org:" + claims.get("org").asString());
+        System.out.println("exp:" + claims.get("exp").asDate());
+}
+ */
+
+
