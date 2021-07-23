12761f20a8e8232c425a8e04ab701b106c0e9060
2594523286@qq.com <2594523286@qq.com>
2021-07-23 17:32:42 +0800

    更新sql脚本

--- src/main/resources/sql/jwt.sql
+++ src/main/resources/sql/jwt.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50515
+Source Server         : 127.0.0.1
+Source Server Version : 50732
 Source Host           : localhost:3306
 Source Database       : jwt
 
 Target Server Type    : MYSQL
-Target Server Version : 50515
+Target Server Version : 50732
 File Encoding         : 65001
 
-Date: 2020-05-24 01:52:00
+Date: 2021-07-23 17:30:44
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -57,6 +57,49 @@ INSERT INTO `dept` VALUES ('6', '1', '黑龙江省', '1');
 INSERT INTO `dept` VALUES ('7', '1', '江苏省', '1');
 INSERT INTO `dept` VALUES ('8', '1', '浙江省', '1');
 INSERT INTO `dept` VALUES ('9', '1', '安徽省', '1');
+
+-- ----------------------------
+-- Table structure for filesmanage
+-- ----------------------------
+DROP TABLE IF EXISTS `filesmanage`;
+CREATE TABLE `filesmanage` (
+  `id` varchar(36) NOT NULL COMMENT '主键',
+  `filename` varchar(255) DEFAULT NULL COMMENT '文件名',
+  `savename` varchar(255) DEFAULT NULL COMMENT '文件存储名称',
+  `filepath` varchar(255) DEFAULT NULL COMMENT '文件存储路径',
+  `creater` varchar(255) DEFAULT NULL COMMENT '创建人',
+  `createime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of filesmanage
+-- ----------------------------
+INSERT INTO `filesmanage` VALUES ('1', '测试', null, null, null, null);
+
+-- ----------------------------
+-- Table structure for sys_task
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_task`;
+CREATE TABLE `sys_task` (
+  `id` varchar(100) NOT NULL,
+  `job_name` varchar(255) DEFAULT NULL COMMENT '任务名',
+  `description` varchar(255) DEFAULT NULL COMMENT '任务描述',
+  `cron_expression` varchar(255) DEFAULT NULL COMMENT 'cron表达式',
+  `bean_class` varchar(255) DEFAULT NULL COMMENT '任务执行时调用哪个类的方法 包名+类名',
+  `job_status` varchar(255) DEFAULT NULL COMMENT '任务状态',
+  `job_group` varchar(255) DEFAULT NULL COMMENT '任务分组',
+  `create_user` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(64) DEFAULT NULL COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_task
+-- ----------------------------
+INSERT INTO `sys_task` VALUES ('1', 'QuertzTask1', 'Quertz定时器测试一', '0/10 * * * * ?', 'com.example.demo.Quertz.TestQuertzTask', '1', 'group', null, null, null, null);
 
 -- ----------------------------
 -- Table structure for user
